660f7035dc916ada2636f74c110546bd308fdd8a
aquinolu25 <quinnpw@live.com>
2025-06-03 23:10:47 -0300

    create class game

---
+++ src/main/kotlin/Jogo.kt
@@ -0,0 +1,15 @@
+package org.example
+
+class Jogo {
+    var titulo = ""
+    var capa = ""
+    val descricao = ""
+
+    override fun toString(): String {
+        return "Meu jogo:\n" +
+                "Título: $titulo,\n" +
+                "Capa: $capa \n" +
+                "Descricao: $descricao"
+    }
+
+}

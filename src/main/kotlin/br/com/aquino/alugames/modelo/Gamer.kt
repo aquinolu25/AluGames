5bb0ecf85afcd6d3ef91545306e84cfa5a7381d2
aquinolu25 <quinnpw@live.com>
2025-06-15 13:05:10 -0300

    Created the Gamer class and the testeGamer file to test Kotlin scope functions

---
+++ src/main/kotlin/br/com/aquino/alugames/modelo/Gamer.kt
@@ -0,0 +1,18 @@
+package br.com.aquino.alugames.modelo
+
+data class Gamer(var nome: String, var email: String) {
+    var dataNascimento: String? = null
+    var usuario: String? = null
+    var idInterno: String? = null
+
+    constructor(nome: String, email: String, dataNascimento: String, usuario: String): this(nome, email) {
+        this.dataNascimento = dataNascimento
+        this.usuario = usuario
+    }
+
+    override fun toString(): String {
+        return "Gamer(nome='$nome', email='$email', dataNascimento=$dataNascimento, usuario=$usuario, idInterno=$idInterno)"
+    }
+
+
+}
---
+++ src/main/kotlin/br/com/aquino/alugames/principal/TesteGamer.kt
@@ -0,0 +1,17 @@
+import br.com.aquino.alugames.modelo.Gamer
+
+fun main() {
+    val gamer1 = Gamer("Ana Paula", "anapaulaa@hotmail.com")
+    println(gamer1)
+
+    val gamer2 = Gamer("Luiz", "luiz59@hotmail.com", "30/10/1997", "aquinolu")
+    println(gamer2)
+
+    gamer1.let {
+        it.dataNascimento = "30/12/1985"
+        it.usuario = "anaapaulaa"
+        it.idInterno = "194150"
+    }
+
+    println(gamer1)
+}

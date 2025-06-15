f5742298dfafc0b47b647735b5a319252a8b21ba
aquinolu25 <quinnpw@live.com>
2025-06-15 14:10:43 -0300

    Created a new function to generate internal ID

--- src/main/kotlin/br/com/aquino/alugames/modelo/Gamer.kt
+++ src/main/kotlin/br/com/aquino/alugames/modelo/Gamer.kt
@@ -1,5 +1,7 @@
 package br.com.aquino.alugames.modelo
 
+import kotlin.random.Random
+
 data class Gamer(var nome: String, var email: String) {
     var dataNascimento: String? = null
     var usuario: String? = null
@@ -8,11 +10,17 @@ data class Gamer(var nome: String, var email: String) {
     constructor(nome: String, email: String, dataNascimento: String, usuario: String): this(nome, email) {
         this.dataNascimento = dataNascimento
         this.usuario = usuario
+        criaIdInterno()
     }
 
     override fun toString(): String {
         return "Gamer(nome='$nome', email='$email', dataNascimento=$dataNascimento, usuario=$usuario, idInterno=$idInterno)"
     }
 
+    fun criaIdInterno() {
+        val numero = Random.nextInt(10000)
+        val tag = String.format("%04d", numero)
+        idInterno = "$usuario#$tag"
+    }
 
 }

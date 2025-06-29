e80a1136a4836ca6ade4f1b3b3a51eaa76b053a5
aquinolu25 <quinnpw@live.com>
2025-06-29 14:35:21 -0300

    Applied the same changes in the Jogo class

--- src/main/kotlin/br/com/aquino/alugames/modelo/Jogo.kt
+++ src/main/kotlin/br/com/aquino/alugames/modelo/Jogo.kt
@@ -1,14 +1,24 @@
 package org.example.br.com.aquino.alugames.modelo
 
+import br.com.aquino.alugames.modelo.Recomendavel
 
-data class Jogo(val titulo:String, val capa:String) {
+
+data class Jogo(val titulo:String, val capa:String): Recomendavel {
     var descricao: String? = null
     var preco = 0.0
+    private val listaNotas = mutableListOf<Int>()
 
     constructor(titulo: String, capa: String, preco: Double, descricao: String):
             this(titulo, capa) {
         this.preco = preco
         this.descricao = descricao
+    }
+
+    override val media: Double
+        get() = listaNotas.average()
+
+    override fun recomendar(nota: Int) {
+        listaNotas.add(nota)
     }
 
     override fun toString(): String {

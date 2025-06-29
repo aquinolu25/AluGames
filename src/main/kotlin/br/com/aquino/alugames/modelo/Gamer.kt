e8e131e476b09cd545c77f909fc33deedaa31a33
aquinolu25 <quinnpw@live.com>
2025-06-29 00:10:28 -0300

    Refactored the code and started introducing the concept of plans in the application

--- src/main/kotlin/br/com/aquino/alugames/modelo/Gamer.kt
+++ src/main/kotlin/br/com/aquino/alugames/modelo/Gamer.kt
@@ -16,6 +16,7 @@ data class Gamer(var nome: String, var email: String) {
     var idInterno: String? = null
         private set
 
+    var plano: PlanoAvulso = PlanoAvulso("BRONZE")
     val jogosBuscados = mutableListOf<Jogo?>()
     val jogosAlugados = mutableListOf<Aluguel>()
 
@@ -55,6 +56,12 @@ data class Gamer(var nome: String, var email: String) {
         val aluguel = Aluguel(this, jogo, periodo)
         jogosAlugados.add(aluguel)
         return aluguel
+    }
+
+    fun jogosDoMes(mes:Int): List<Jogo> {
+        return jogosAlugados
+            .filter { aluguel ->  aluguel.periodo.dataInicial.monthValue == mes}
+            .map { aluguel ->  aluguel.jogo}
     }
 
     companion object {
